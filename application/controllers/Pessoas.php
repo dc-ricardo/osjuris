00d901f4c7988df87db7dc5b38dd2629cd04f12f
dc-ricardo <dc.ricardo@gmail.com>
2016-01-04 01:13:25 -0200

    Modificada rotina de validação de inserção e edição de Pessoas

--- application/controllers/Pessoas.php
+++ application/controllers/Pessoas.php
@@ -18,42 +18,134 @@ public function novo() {
 	$this->load->view('includes/vfooter');
 }
 
+function vpostados() {
+	$data =	array(
+		'codigo' => $this->input->post('codigo'),
+		'tipo' => $this->input->post('tipo'),
+		'cpf_cnpj' => $this->input->post('cpf_cnpj'),
+		'rg_ie' => $this->input->post('rg_ie'),
+		'nome_razao' => $this->input->post('nome_razao'),
+		'endereco' => $this->input->post('endereco'),
+		'numero' => $this->input->post('numero'),
+		'complemento' => $this->input->post('complemento'),
+		'bairro' => $this->input->post('bairro'),
+		'cidade' => $this->input->post('cidade'),
+		'estado' => $this->input->post('estado'),
+		'cep' => $this->input->post('cep'),
+		'telefone' => $this->input->post('telefone'),
+		'celular' => $this->input->post('celular'),
+		'email' => $this->input->post('email'),
+		'observacoes' => $this->input->post('observacoes')
+	);
+	return $data;
+}
+
+function regras($operacao) {
+  $vrules['padrao'] = array(
+		array(
+			'field' => 'rg_ie',
+			'label' => 'RG/Inscrição',
+			'rules' => 'max_length[20]|alpha_numeric|trim'
+		),
+		array(
+			'field' => 'endereco',
+			'label' => 'Endereço',
+			'rules' => 'max_length[80]|trim'
+		),
+		array(
+			'field' => 'complemento',
+			'label' => 'Complemento',
+			'rules' => 'max_length[20]|trim'
+		),
+		array(
+			'field' => 'bairro',
+			'label' => 'Bairro',
+			'rules' => 'max_length[60]|trim'
+		),
+		array(
+			'field' => 'cidade',
+			'label' => 'Cidade',
+			'rules' => 'max_length[60]|trim'
+		),
+		array(
+			'field' => 'estado',
+			'label' => 'Estado',
+			'rules' => 'max_length[2]|trim'
+		),
+		array(
+			'field' => 'cep',
+			'label' => 'CEP',
+			'rules' => 'max_length[8]|trim'
+		),
+		array(
+			'field' => 'telefone',
+			'label' => 'Telefone',
+			'rules' => 'max_length[20]|trim'
+		),
+		array(
+			'field' => 'celular',
+			'label' => 'Celular',
+			'rules' => 'max_length[20]|trim'
+		),
+		array(
+			'field' => 'email',
+			'label' => 'E-mail',
+			'rules' => 'max_length[60]|trim'
+		),
+		array(
+			'field' => 'numero',
+			'label' => 'Número',
+			'rules' => 'integer'
+		)
+	);
+
+	$vrules['insercao'] = array(
+		array(
+			'field' => 'nome_razao',
+			'label' => 'Nome/Razão Social',
+			'rules' => 'required|is_unique[pessoas.nome_razao]|max_length[80]|trim'
+		),
+		array(
+			'field' => 'codigo',
+			'label' => 'Código',
+			'rules' => 'required|is_unique[pessoas.codigo]|exact_length[6]'
+		),
+		array(
+			'field' => 'cpf_cnpj',
+			'label' => 'CPF/CNPJ',
+			'rules' => 'required|is_unique[pessoas.cpf_cnpj]|max_length[20]|alpha_numeric'
+		)
+	);
+
+	$vrules['alteracao'] = array(
+		array(
+			'field' => 'nome_razao',
+			'label' => 'Nome/Razão Social',
+			'rules' => 'required|max_length[80]|trim'
+		),
+		array(
+			'field' => 'codigo',
+			'label' => 'Código',
+			'rules' => 'required|exact_length[6]'
+		),
+		array(
+			'field' => 'cpf_cnpj',
+			'label' => 'CPF/CNPJ',
+			'rules' => 'required|max_length[20]|alpha_numeric'
+		)
+	);
+
+	$vrules = array_merge($vrules['padrao'], $vrules[$operacao]);
+
+	return $vrules;
+}
+
 public function insere() {
 	$this->load->library('form_validation');
-	$this->form_validation->set_rules('nome_razao','Nome/Razão Social','required|is_unique[pessoas.nome_razao]|max_length[80]|trim');
-	$this->form_validation->set_rules('codigo','Código','required|is_unique[pessoas.codigo]|exact_length[6]');
-	$this->form_validation->set_rules('cpf_cnpj','CPF/CNPJ','required|is_unique[pessoas.cpf_cnpj]|max_length[20]|alpha_numeric');
-	$this->form_validation->set_rules('rg_ie','RG/Inscrição','max_length[20]|alpha_numeric|trim');
-	$this->form_validation->set_rules('endereco','Endereço','max_length[80]|trim');
-	$this->form_validation->set_rules('complemento','Complemento','max_length[20]|trim');
-	$this->form_validation->set_rules('bairro','Bairro','max_length[60]|trim');
-	$this->form_validation->set_rules('cidade','Cidade','max_length[60]|trim');
-	$this->form_validation->set_rules('estado','Estado','max_length[2]');
-	$this->form_validation->set_rules('cep','CEP','max_length[8]');
-	$this->form_validation->set_rules('telefone','Telefone','max_length[20]|trim');
-	$this->form_validation->set_rules('celular','Celular','max_length[20]|trim');
-	$this->form_validation->set_rules('email','E-mail','valid_email|max_length[60]|trim');
-	$this->form_validation->set_rules('numero','Número','integer');
+	$this->form_validation->set_rules($this->regras('insercao'));
 
   if ($this->form_validation->run() == TRUE) {
-	  $data = array(
-				'codigo' => $this->input->post('codigo'),
-				'tipo' => $this->input->post('tipo'),
-				'cpf_cnpj' => $this->input->post('cpf_cnpj'),
-				'rg_ie' => $this->input->post('rg_ie'),
-				'nome_razao' => $this->input->post('nome_razao'),
-				'endereco' => $this->input->post('endereco'),
-				'numero' => $this->input->post('numero'),
-				'complemento' => $this->input->post('complemento'),
-				'bairro' => $this->input->post('bairro'),
-				'cidade' => $this->input->post('cidade'),
-				'estado' => $this->input->post('estado'),
-				'cep' => $this->input->post('cep'),
-				'telefone' => $this->input->post('telefone'),
-				'celular' => $this->input->post('celular'),
-				'email' => $this->input->post('email'),
-				'observacoes' => $this->input->post('observacoes')
-		);
+	  $data = $this->vpostados();
 
 		$this->load->model('mpessoas');
 	  $this->mpessoas->insere($data);
@@ -86,28 +178,10 @@ public function edita($id) {
 
 public function atualiza($id) {
 	$this->load->library('form_validation');
-	$this->form_validation->set_rules('nome_razao','Nome/Razão Social','required|max_length[80]|trim');
+	$this->form_validation->set_rules($this->regras('alteracao'));
 
   if ($this->form_validation->run() == TRUE) {
-	  $data = array(
-				'codigo' => $this->input->post('codigo'),
-				'tipo' => $this->input->post('tipo'),
-				'cpf_cnpj' => $this->input->post('cpf_cnpj'),
-				'rg_ie' => $this->input->post('rg_ie'),
-				'nome_razao' => $this->input->post('nome_razao'),
-				'endereco' => $this->input->post('endereco'),
-				'numero' => $this->input->post('numero'),
-				'complemento' => $this->input->post('complemento'),
-				'bairro' => $this->input->post('bairro'),
-				'cidade' => $this->input->post('cidade'),
-				'estado' => $this->input->post('estado'),
-				'cep' => $this->input->post('cep'),
-				'telefone' => $this->input->post('telefone'),
-				'celular' => $this->input->post('celular'),
-				'email' => $this->input->post('email'),
-				'observacoes' => $this->input->post('observacoes')
-		);
-
+	  $data = $this->vpostados();
 		$this->load->model('mpessoas');
 		$this->mpessoas->atualiza($id, $data);
 		redirect('/pessoas/consulta/'.$id);
@@ -115,7 +189,6 @@ public function atualiza($id) {
 	else {
 		$this->edita($id);
 	}
-
 }
 
 }
--- application/views/includes/vheader.php
+++ application/views/includes/vheader.php
@@ -48,6 +48,7 @@
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
               <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+              <span class="caret"></span>    
             </a>
             <ul class="dropdown-menu">
               <li><a href="<?=base_url()?>perfil">Perfil</a></li>
